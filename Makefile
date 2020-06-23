a7618cb796ba9e07d46045a8a947b5870f14a24c
Jose Septien <jaseptienh@gmail.com>
2020-06-23 13:13:52 -0500

    Add tests for all ciphers.

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC = /usr/bin/gcc
 
-LDFLAGS=-L/home/pi/Documents/Maestria/Tesis/PQSPerformance#/Users/jash/Documents/Maestria/Tesis/performanceTest/newhope
-LIBFLAGS=-lnewhope
+LDFLAGS=-L/media/phantom/Data/Crypto/PQSPerformance#/home/pi/Documents/Maestria/Tesis/PQSPerformance#/Users/jash/Documents/Maestria/Tesis/performanceTest/newhope
+LIBFLAGS=-lnewhope -lntru -lntru-prime
 PERFFLAGS=-O3 -fomit-frame-pointer -march=native
-CFLAGS=-DRPI #For the raspberry pi
+CFLAGS= #-DRPI #For the raspberry pi
 
 SOURCES=main.c performance.c
-HEADERS=performance.h newhope/api.h
+HEADERS=performance.h newhope/api.h ntru/api.h
 
 test: $(SOURCES) $(HEADERS)
 	$(CC) $(CFLAGS) $(LDFLAGS) $(SOURCES) -o $@ $(LIBFLAGS) $(PERFFLAGS)
--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #include "performance.h"
 
 // Crypto libraries
-#include "newhope/api.h"
+
 
 #ifdef RPI
 #define uint64_t u_int64_t
@@ -28,65 +28,209 @@ int comp(const void *elem1, const void *elem2)
     return 0;
 }
 
-void testNewHope(int N, uint64_t *medians, uint64_t *t_keygen, uint64_t *t_dec, uint64_t *t_enc)
+void testNewHope(int N, struct values **means, struct values **keygen, struct values **dec, struct values **enc)
 {
+//#include "newhope/api.h"
+    // For measuring time
+    int i;
+
+    // For the scheme
+   /* unsigned char pk[CRYPTO_PUBLICKEYBYTES], sk[CRYPTO_SECRETKEYBYTES], ss[CRYPTO_BYTES], ct[CRYPTO_CIPHERTEXTBYTES];
+
+   for (i = 0; i < N; i++)
+    {
+        // Key generation
+        testKeyGen(crypto_kem_keypair, pk, sk, keygen[i]);
+        // Encapsulation
+        testEnc(crypto_kem_enc, ct, ss, pk, enc[i]);
+        // Decapsulation
+        testDec(crypto_kem_dec, ss, ct, sk, dec[i]);
+    }
+
+    means[0]->cycles = means[0]->time = 0;
+    means[1]->cycles = means[1]->time = 0;
+    means[2]->cycles = means[2]->time = 0;
+    for (i = 0; i < N; i++)
+    {
+        means[0]->cycles += keygen[i]->cycles;
+        means[0]->time += keygen[i]->time;
+        means[1]->cycles += enc[i]->cycles;
+        means[1]->time += enc[i]->time;
+        means[2]->cycles += dec[i]->cycles;
+        means[2]->time += dec[i]->time;
+    }
+    means[0]->cycles /= N;
+    means[0]->time /= N;
+    means[1]->cycles /= N;
+    means[1]->time /= N;
+    means[2]->cycles /= N;
+    means[2]->time /= N;*/
+}
+
+void testNTRU(int N, struct values **means, struct values **keygen, struct values **dec, struct values **enc)
+{
+/*#include "ntru/api.h"
     // For measuring time
     int i;
 
     // For the scheme
     unsigned char pk[CRYPTO_PUBLICKEYBYTES], sk[CRYPTO_SECRETKEYBYTES], ss[CRYPTO_BYTES], ct[CRYPTO_CIPHERTEXTBYTES];
 
-    for (i = 0; i < N; i++)
+       for (i = 0; i < N; i++)
     {
         // Key generation
-        t_keygen[i] = testKeyGen(crypto_kem_keypair, pk, sk);
+        testKeyGen(crypto_kem_keypair, pk, sk, keygen[i]);
         // Encapsulation
-        t_enc[i] = testEnc(crypto_kem_enc, ct, ss, pk);
+        testEnc(crypto_kem_enc, ct, ss, pk, enc[i]);
         // Decapsulation
-        t_dec[i] = testDec(crypto_kem_dec, ss, ct, sk);
+        testDec(crypto_kem_dec, ss, ct, sk, dec[i]);
     }
 
-    // Sort arrays
-    qsort(t_keygen, sizeof(t_keygen) / sizeof(*t_keygen), sizeof(*t_keygen), comp);
-    qsort(t_enc, sizeof(t_enc) / sizeof(*t_enc), sizeof(*t_enc), comp);
-    qsort(t_dec, sizeof(t_dec) / sizeof(*t_dec), sizeof(*t_dec), comp);
-
-    // The medians
-    medians[0] = t_keygen[(int)N/2];
-    medians[1] = t_enc[(int)N/2]; 
-    medians[2] = t_dec[(int)N/2];
+    means[0]->cycles = means[0]->time = 0;
+    means[1]->cycles = means[1]->time = 0;
+    means[2]->cycles = means[2]->time = 0;
+    for (i = 0; i < N; i++)
+    {
+        means[0]->cycles += keygen[i]->cycles;
+        means[0]->time += keygen[i]->time;
+        means[1]->cycles += enc[i]->cycles;
+        means[1]->time += enc[i]->time;
+        means[2]->cycles += dec[i]->cycles;
+        means[2]->time += dec[i]->time;
+    }
+    means[0]->cycles /= N;
+    means[0]->time /= N;
+    means[1]->cycles /= N;
+    means[1]->time /= N;
+    means[2]->cycles /= N;
+    means[2]->time /= N;*/
 }
 
-void makeTest(int N, uint64_t *medians, uint64_t *keygen, uint64_t *dec, uint64_t *enc)
+void testNTRUprime(int N, struct values **means, struct values **keygen, struct values **dec, struct values **enc)
 {
-    testNewHope(N, medians, keygen, dec, enc);
+/*#include "ntru-prime/api.h"
+#include "ntru-prime/crypto_kem.h"
+    // For measuring time
+    int i;
+
+    // For the scheme
+    unsigned char pk[CRYPTO_PUBLICKEYBYTES], sk[CRYPTO_SECRETKEYBYTES], ss[CRYPTO_BYTES], ct[CRYPTO_CIPHERTEXTBYTES];
+
+       for (i = 0; i < N; i++)
+    {
+        // Key generation
+        testKeyGen(crypto_kem_keypair, pk, sk, keygen[i]);
+        // Encapsulation
+        testEnc(crypto_kem_enc, ct, ss, pk, enc[i]);
+        // Decapsulation
+        testDec(crypto_kem_dec, ss, ct, sk, dec[i]);
+    }
+
+    means[0]->cycles = means[0]->time = 0;
+    means[1]->cycles = means[1]->time = 0;
+    means[2]->cycles = means[2]->time = 0;
+    for (i = 0; i < N; i++)
+    {
+        means[0]->cycles += keygen[i]->cycles;
+        means[0]->time += keygen[i]->time;
+        means[1]->cycles += enc[i]->cycles;
+        means[1]->time += enc[i]->time;
+        means[2]->cycles += dec[i]->cycles;
+        means[2]->time += dec[i]->time;
+    }
+    means[0]->cycles /= N;
+    means[0]->time /= N;
+    means[1]->cycles /= N;
+    means[1]->time /= N;
+    means[2]->cycles /= N;
+    means[2]->time /= N;*/
+}
+
+void testSaber(int N, struct values **means, struct values **keygen, struct values **dec, struct values **enc)
+{
+#include "saber/api.h"
+    // For measuring time
+    int i;
+
+    // For the scheme
+    unsigned char pk[CRYPTO_PUBLICKEYBYTES], sk[CRYPTO_SECRETKEYBYTES], ss[CRYPTO_BYTES], ct[CRYPTO_CIPHERTEXTBYTES];
+
+       for (i = 0; i < N; i++)
+    {
+        // Key generation
+        testKeyGen(crypto_kem_keypair, pk, sk, keygen[i]);
+        // Encapsulation
+        testEnc(crypto_kem_enc, ct, ss, pk, enc[i]);
+        // Decapsulation
+        testDec(crypto_kem_dec, ss, ct, sk, dec[i]);
+    }
+
+    means[0]->cycles = means[0]->time = 0;
+    means[1]->cycles = means[1]->time = 0;
+    means[2]->cycles = means[2]->time = 0;
+    for (i = 0; i < N; i++)
+    {
+        means[0]->cycles += keygen[i]->cycles;
+        means[0]->time += keygen[i]->time;
+        means[1]->cycles += enc[i]->cycles;
+        means[1]->time += enc[i]->time;
+        means[2]->cycles += dec[i]->cycles;
+        means[2]->time += dec[i]->time;
+    }
+    means[0]->cycles /= N;
+    means[0]->time /= N;
+    means[1]->cycles /= N;
+    means[1]->time /= N;
+    means[2]->cycles /= N;
+    means[2]->time /= N;
+}
+
+void makeTest(int N, struct values **means, struct values **keygen, struct values **dec, struct values **enc)
+{
+    //estNewHope(N, means, keygen, dec, enc);
+    //testNTRU(N, means, keygen, dec, enc);
+    //testNTRUprime(N, means, keygen, dec, enc);
+    testSaber(N, means, keygen, dec, enc);
 }
 
 int main(void)
 {
-    uint64_t *medians, *keygen, *enc, *dec;
+
+    struct values **keygen, **enc, **dec, **means;
     int N = 2000;
-    medians = (uint64_t *)malloc(3 * sizeof(uint64_t));
-    keygen = (uint64_t *)malloc(N * sizeof(uint64_t));
-    enc = (uint64_t *)malloc(N * sizeof(uint64_t));
-    dec = (uint64_t *)malloc(N * sizeof(uint64_t));
+    keygen = (struct values **)malloc(N * sizeof(struct values *));
+    enc = (struct values **)malloc(N * sizeof(struct values *));
+    dec = (struct values **)malloc(N * sizeof(struct values *));
+    means = (struct values **)malloc(3 * sizeof(struct values *));
+
+    /* Allocate memory for each entry */
+    for (int i = 0; i < N; i++)
+    {
+        keygen[i] = (struct values *) malloc (sizeof(struct values));
+        enc[i] = (struct values *) malloc (sizeof(struct values));
+        dec[i] = (struct values *) malloc (sizeof(struct values));
+    }
+    means[0] = (struct values *)malloc(sizeof(struct values));
+    means[1] = (struct values *)malloc(sizeof(struct values));
+    means[2] = (struct values *)malloc(sizeof(struct values));
     
 #ifdef RPI    
     init_module();
 #endif
 
-    makeTest(2000, medians, keygen, dec, enc);
-    printf("Median for the KeyGen function:\n\t%llu\n", medians[0]);
-    printf("Median for the Enc function:\n\t%llu\n", medians[1]);
-    printf("Median for the Dec function:\n\t%llu\n", medians[2]);
+    makeTest(2000, means, keygen, dec, enc);
+
+    printf("Mean for the KeyGen function:\n\t%f\t%f\n", means[0]->cycles, means[0]->time);
+    printf("Mean for the Enc function:\n\t%f\t%f\n", means[1]->cycles, means[1]->time);
+    printf("Mean for the Dec function:\n\t%f\t%f\n", means[2]->cycles, means[2]->time);
 
     int i;
-    FILE *pfile;
-    pfile = fopen("timemeasure.csv", "w");
-    fprintf(pfile, "%llu, %llu, %llu\n", medians[0], medians[1], medians[2]);
+    //FILE *pfile;
+    /*pfile = fopen("timemeasure.csv", "w");
+    fprintf(pfile, "%lu, %lu, %lu\n", medians[0], medians[1], medians[2]);
     for (int i = 0; i < N-1; i++)
     {
-        fprintf(pfile, "%llu,%llu,%llu\n", keygen[i], enc[i], dec[i]);
-    }
+        fprintf(pfile, "%lu,%lu,%lu\n", keygen[i], enc[i], dec[i]);
+    }*/
     return 0;
 }
