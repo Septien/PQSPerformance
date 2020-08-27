6227bd5a06039fff446dbfa541309c063c75002d
Jose Septien <jaseptienh@gmail.com>
2020-08-27 18:26:40 -0500

    Add program to automatically measure the memory consumption of the ciphers.

---
+++ measureMemory.py
@@ -0,0 +1,21 @@
+import os
+
+ciphers = ["NTRU=1", "SABER=1", "KYBER=1", "FRODO=1"]
+massiffile = ["ntru.out", "saber.out", "kyber.out", "frodo.out"]
+rm = "rm test"
+make = "make test "
+valgrind = "valgrind --tool=massif --heap=yes --massif-out-file="
+
+for i in range(5):
+    print(ciphers[i])
+    # Remove test binary
+    cmd = rm
+    os.system(cmd)
+
+    # make with the memory option enabled
+    cmd = make + ciphers[i] + " MEMORY=1"
+    os.system(cmd)
+
+    # Profile with valgrind
+    cmd = valgrind + massiffile[i] + " ./test"
+    os.system(cmd)
