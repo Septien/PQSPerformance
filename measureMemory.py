8cf9f5feaa7ca77d5d96e4de6cfa9f1850c45a4a
Jose Septien <jaseptienh@gmail.com>
2020-09-03 13:21:15 -0500

    Complete script for measuring the memory.

--- measureMemory.py
+++ measureMemory.py
@@ -1,21 +1,77 @@
 import os
+import csv
+import numpy as np
 
 ciphers = ["NTRU=1", "SABER=1", "KYBER=1", "FRODO=1"]
-massiffile = ["ntru.out", "saber.out", "kyber.out", "frodo.out"]
+massiffile = ["ntru/ntru", "saber/saber", "kyber/kyber", "frodo/frodo"]
+ext = ".out"
+memory = "memory/"
 rm = "rm test"
 make = "make test "
-valgrind = "valgrind --tool=massif --heap=yes --massif-out-file="
+valgrind = "valgrind --tool=massif --stacks=yes --massif-out-file="
+N = 2000
 
-for i in range(5):
-    print(ciphers[i])
-    # Remove test binary
-    cmd = rm
-    os.system(cmd)
+def measureMemory():
+    # For each cipher, measure its memory consumption
+    for i in range(4):
+        for j in range(N):
+            # Remove test binary
+            os.system(rm)
 
-    # make with the memory option enabled
-    cmd = make + ciphers[i] + " MEMORY=1"
-    os.system(cmd)
+            # make with the memory option enabled
+            cmd = make + ciphers[i] + " MEMORY=1 DEBUG=1"
+            os.system(cmd)
 
-    # Profile with valgrind
-    cmd = valgrind + massiffile[i] + " ./test"
-    os.system(cmd)
+            # Profile memory with valgrind
+            cmd = valgrind + memory + massiffile[i] + "_" + str(j) + ext + " ./test"
+            os.system(cmd)
+
+def readThreeLines(file):
+    file.readline()
+    file.readline()
+    file.readline()
+
+def getTotalMemory(file):
+    heap = int(file.readline().split("=")[1])
+    heapExtra = int(file.readline().split("=")[1])
+    stack = int(file.readline().split("=")[1])
+    return heap + heapExtra + stack
+
+def getMemoryUsage():
+    # Read the generated output file, and get the maximum amount of memory used
+    memUsageAll = []
+    for i in range(4):
+        memUsage = []   # Stores the max values for each file
+        for j in range(N):
+            fileN = memory + massiffile[i] + "_" + str(j) + ext
+            maxM = 0
+            with open(fileN, "r") as file:
+                # Read the first three lines:
+                readThreeLines(file)
+                # Find the max value
+                for line in file:
+                    # Search the following '#' character
+                    if line[0] == "#":
+                        # Read the following three lines
+                        readThreeLines(file)
+                        # Get the total amount of memory
+                        total = getTotalMemory(file)
+                        if (total > maxM):
+                            maxM = total
+            memUsage.append(maxM)
+        memUsageAll.append(memUsage.copy())
+    return memUsageAll
+
+def saveData(data, file, delimiter):
+    m = np.array(data)
+    mT = m.transpose()
+    with open(file, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=delimiter)
+        cipherS = [c.split("=")[0] for c in ciphers]
+        writer.writerow(cipherS)
+        writer.writerows(mT)
+
+if __name__ == '__main__':
+    measureMemory()
+    m = getMemoryUsage()
+    saveData(m, "memoryPerformance.csv", ",")
